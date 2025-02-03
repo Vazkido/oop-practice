739223975fc60dad0bfef07a7caccf0deae77509
vazkido <vazkid02@gmail.com>
2025-02-03 08:01:06 -0500

    js practice2

--- practice.js
+++ practice.js
@@ -1,33 +1,47 @@
-class Animal {
-  //Constructor method to initialize object properties
-  constructor(name, color, isWild, sound) {
-    this.name = name;
-    this.color = color;
-    this.isWild = isWild;
-    this.sound = sound;
-  }
-  // you can add your methods here
-  makeSound() {
-    console.log(`${this.name} ${this.sound}`);
+// class Animal {
+//   //Constructor method to initialize object properties
+//   constructor(name, color, isWild, sound) {
+//     this.name = name;
+//     this.color = color;
+//     this.isWild = isWild;
+//     this.sound = sound;
+//   }
+//   // you can add your methods here
+//   makeSound() {
+//     console.log(`${this.name} ${this.sound}`);
+//   }
+// }
+
+// // INHERITANCE
+// class AnimalClassification extends Animal {
+//   //constructor method to initialize object properties
+//   constructor(name, order) {
+//     super(name);
+//     this.order = order;
+//   }
+// }
+
+// classifyByOrder(animalName) {
+//     const animalClass = animals.find((animal) => animal.name === animalName)
+
+//     if (animalClass){
+//       console.log(`${animalClass.name} is a ${animalClass.order}`);
+//     }else{
+//       console.log(`${animalName} class not found`);
+//     }
+//     return this;
+// }
+
+
+//POLYMORPHISM EXAMPLE 
+class Shape {
+  area() {
+    console.log("Calculating area of a shape");
+    
   }
 }
-
-// INHERITANCE
-class AnimalClassification extends Animal {
-  //constructor method to initialize object properties
-  constructor(name, order) {
-    super(name);
-    this.order = order;
+class Square extends Shape {
+  area(){
+    console.log("Calculating area of a square"); 
   }
-}
-
-classifyByOrder(animalName) {
-    const animalClass = animals.find((animal) => animal.name === animalName)
-
-    if (animalClass){
-      console.log(`${animalClass.name} is a ${animalClass.order}`);
-    }else{
-      console.log(`${animalName} class not found`);
-    }
-    return this;
 }
